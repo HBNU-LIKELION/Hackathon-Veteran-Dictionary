f47817bd85c9c613cc7f7c46562851d6b647939d
PlagueRabbit <jkim826@naver.com>
2023-07-26 20:58:08 +0900

    add requirements,local hosts

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-nq=y(6bypuuz=o+5#g7qcozdbb!-d9zkln8$exrx3)$w#y*pik
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['43.202.46.120']
 
 
 # Application definition
