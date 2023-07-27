35538e164f102dd3a0ed0112dfe550e7f1a45f06
PlagueRabbit <jkim826@naver.com>
2023-07-27 18:52:24 +0900

    url 더러운거 수정

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,6 @@ schema_view = get_schema_view(title='API 목록', version='1.0.0')
 urlpatterns = [
     path('', schema_view),
     path('admin/', admin.site.urls),
-    path('api/wiki/', include('wiki.urls')),# 앱 폴더 내의 urls.py로 URL 패턴을 넘겨줌
-    path('api/account/',include('account.urls'))
+    path('api/', include('wiki.urls')),# 앱 폴더 내의 urls.py로 URL 패턴을 넘겨줌
+    path('account/',include('account.urls'))
 ]
